eda3634373f85ab46a9ef27b08ebcb5b578cffd5
Shahrukh777 <sheikhshahrukh904@gmail.com>
2024-06-20 16:25:59 +0530

    IIFE DESC

--- IIFE.js
+++ IIFE.js
@@ -1,6 +1,6 @@
 
 // IMMIDIATELY INVOKED FUNCTION EXPRESSION (IIFE)
-
+// FUNCTION EXECUTION AS SOON AS ITS DECLARED/DEFINED
 (function chai(){
     console.log("This is IIFE")  // UNNAMED IIFE
 })();                            // USE ; FOR STOPPING CONTEXT OR ENDING
